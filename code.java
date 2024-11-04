039b5fce82d15328ae6e7899f247527d642a10a4
AshishJha236 <=>
2024-11-05 03:26:15 +0530

    Updated With the Conversion Code

--- code.java
+++ code.java
@@ -1,3 +1,4 @@
+//This code is for 2D Matrix to make satisfiable the conditions. 
 class Solution {
     public boolean satisfiesConditions(int[][] grid) {
         int row = grid.length;
@@ -23,4 +24,50 @@ class Solution {
 
         
     }
+}
+ 
+
+
+
+// This code for Converting String Into an Integer.
+class Solution {
+    public static int myAtoi(String s) {
+            int len = s.length();
+            long num = 0; // Use long to handle overflow before converting to int
+            int i = 0;
+            long max = Integer.MAX_VALUE;
+            long min = Integer.MIN_VALUE;
+
+            // Skip leading whitespace
+            while (i < len && s.charAt(i) == ' ') {
+                i++;
+            }
+
+            // Check for sign
+            int negative = 1;
+            if (i < len && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+                negative = s.charAt(i) == '-' ? -1: 1;
+                i++;
+            }
+
+            // Build the number
+            while (i < len && Character.isDigit(s.charAt(i))) {
+                num = num * 10 + (s.charAt(i) - '0');
+                i++;
+
+                // Check for overflow
+                if (negative == 1 && num > max) {
+                    return Integer.MAX_VALUE;
+                }
+                
+                if (negative == -1 && -num < min) {
+                    return Integer.MIN_VALUE;
+                }
+            }
+            // Apply sign
+            num = negative== -1? -num : num;
+
+            return (int) num; // Cast to int before returning
+    }
+
 }
